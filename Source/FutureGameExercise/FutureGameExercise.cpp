164c360e2b2c2c593236bcc79df1a8f552449e8e
dartdaven <139046041+dartdaven@users.noreply.github.com>
2023-10-17 21:33:53 +0400

    Initial commit

---
+++ Source/FutureGameExercise/FutureGameExercise.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FutureGameExercise.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FutureGameExercise, "FutureGameExercise" );
+ 
